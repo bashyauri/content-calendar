a76ff9ea96e8bbe94ff58545ed2555b644eec368
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-12-05 19:10:09 +0100

    initialize first content

--- src/main/java/com/bash/contentcalendar/controller/ContentController.java
+++ src/main/java/com/bash/contentcalendar/controller/ContentController.java
@@ -1,10 +1,37 @@
 package com.bash.contentcalendar.controller;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bash.contentcalendar.model.Content;
+import com.bash.contentcalendar.model.Status;
+
+import com.bash.contentcalendar.model.Type;
+import com.bash.contentcalendar.repository.ContentCollectionRepository;
+
+import jakarta.annotation.PostConstruct;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api/content")
 public class ContentController {
+    private final ContentCollectionRepository repository;
+
+    public ContentController(ContentCollectionRepository repository) {
+        this.repository = repository;
+
+    }
+
+    // make a request and find all the pieces of content in the system
+    @GetMapping("")
+    public List<Content> findAll() {
+        return repository.findAll();
+
+    }
 
 }
--- src/main/java/com/bash/contentcalendar/repository/ContentCollectionRepository.java
+++ src/main/java/com/bash/contentcalendar/repository/ContentCollectionRepository.java
@@ -1,8 +1,40 @@
 package com.bash.contentcalendar.repository;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Repository;
+
+import com.bash.contentcalendar.model.Content;
+import com.bash.contentcalendar.model.Status;
+import com.bash.contentcalendar.model.Type;
+
+import jakarta.annotation.PostConstruct;
 
 @Repository
 public class ContentCollectionRepository {
+    private final List<Content> content = new ArrayList<>();
+
+    public ContentCollectionRepository() {
+
+    }
+
+    public List<Content> findAll() {
+        return content;
+    }
+
+    public Optional<Content> findById(Integer id) {
+        return content.stream().filter(content -> content.id().equals(id)).findFirst();
+    }
+
+    @PostConstruct
+    private void init() {
+        Content c = new Content(
+                1, "My First Blog Post", "My first blog", Status.IDEA, Type.ARTICLES, LocalDateTime.now(),
+                null, "");
+        content.add(c);
+    }
 
 }
